7cd8f87710a8c7414b79445765854c2b9a92a6fa
Nguyễn Trọng Đức <52000646@student.tdtu.edu.vn>
2023-06-13 10:43:48 +0700

    Config Route and create path

---
+++ src/pages/Following/index.js
@@ -0,0 +1,9 @@
+function Following() {
+    return (
+        <div>
+            <h2>This is following page</h2>
+        </div>
+    );
+}
+
+export default Following;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,9 @@
+function Upload() {
+    return (
+        <div>
+            <h1>This is upload page</h1>
+        </div>
+    );
+}
+
+export default Upload;
